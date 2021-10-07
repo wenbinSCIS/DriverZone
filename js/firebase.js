9f7507f4f77d214bebfffb4ebdbee58a650249f6
HoonQiHang <62787336+hoonqh@users.noreply.github.com>
2021-10-07 20:39:44 +0800

    updated

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseApp= initializeApp({
+    apiKey: "AIzaSyDruPuuruiIkp6rGikLAUixFbT5-RFRm0s",
+    authDomain: "wad2-e8948.firebaseapp.com",
+    databaseURL: "https://wad2-e8948-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "wad2-e8948",
+    storageBucket: "wad2-e8948.appspot.com",
+    messagingSenderId: "905590919273",
+    appId: "1:905590919273:web:5d6c7e3da73fbdb9cdd2f8"
+})
