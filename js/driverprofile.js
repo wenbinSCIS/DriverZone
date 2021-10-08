b79e137497d79eca8979a4261ad17f5fa99682e4
HoonQiHang <62787336+hoonqh@users.noreply.github.com>
2021-10-09 01:42:20 +0800

    ff

--- js/driverprofile.js
+++ js/driverprofile.js
@@ -17,30 +17,108 @@
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
 
-    import {getDatabase, ref, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js" 
+    import {getDatabase, ref, set, child, get,update, remove} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js" 
 
     const db= getDatabase();
 
 
-    function add() {
+    // function add() {
 
       
-      var name= document.getElementById("name");
-      var submit=document.getElementById("submit");
+    //   var name= document.getElementById("name");
+    //   var submit=document.getElementById("submit");
         
-        set(ref(db, name.value),{
-            Name: name.value
-        }      
+    //     set(ref(db, name.value),{
+    //         Name: name.value
+    //     }      
         
-        )
-        .then(()=>{
-            alert("data stored successfully");
+    //     )
+    //     .then(()=>{
+    //         alert("data stored successfully");
             
-        })
-        .catch((error)=>{
-            alert("data not stored successfully");
+    //     })
+    //     .catch((error)=>{
+    //         alert("data not stored successfully");
             
+    //     })
+    // }
+
+  
+        // var name= document.getElementById("name");
+        // var submit=document.getElementById("submit");
+
+        const dbref= ref(db);
+
+        get(child(dbref,"instructor")).then((snapshot)=>{
+            if (snapshot.exists()) {
+                return display(snapshot.val());
+            }
+            else {
+                alert("No data found")
+            }
         })
+        // .catch((error)=>{
+        //     alert("unsucessful"+error);
+        // })
+     
+    
+
+
+    
+
+    
+    function display(input) {
+        var body=document.getElementById("body");
+        var arr=[];
+        for (let ele in input) {
+            console.log(ele)
+            var name=ele;
+            var lang=input[ele].language
+            var price=input[ele].price
+            console.log(price)
+            var image="../images/"+name+".jpg";
+            console.log(image)
+            var tr=document.createElement("tr");
+            tr.appendChild(createNode(name));
+            tr.appendChild(createNode(lang));
+            tr.appendChild(createNode(price));
+            tr.appendChild(ImgNode(name));
+            arr.push(tr);
+            console.log(tr)
+            let a=appendBody(tr);
+            // body.appendChild(tr);
+
+            // tr.appendChild(td);
+            // body.appendChild(tr);
+            // console.log(body)
+
+
+            
+            
+            ;
+        }
     }
 
-    submit.addEventListener("click",add)
+    function createNode(input) {
+        var td=document.createElement("td");
+        var name_txt=document.createTextNode(input);
+        td.appendChild(name_txt)
+        return td;
+    }
+
+    function ImgNode(name) {
+        var image="../images/"+name+".jpg";
+        var td=document.createElement("td");
+        var img_node=document.createElement("img");
+        img_node.src=image;
+        img_node.style.width="100px";
+        img_node.style.height="100px";
+        td.appendChild(img_node);
+        return td;
+    }
+
+    function appendBody(input) {
+        console.log(input)
+        var body=document.getElementById("body");
+        body.appendChild(input)
+    }
