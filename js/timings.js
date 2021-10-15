f8256864daa9178ca09a2891d0fda72c8bbd8774
Ho-JE <juayen.ho.2020@scis.smu.edu.sg>
2021-10-15 21:04:01 +0800

    Update timings.js

--- js/timings.js
+++ js/timings.js
@@ -14,38 +14,69 @@ const firebaseConfig = {
     appId: "1:905590919273:web:5d6c7e3da73fbdb9cdd2f8"
 };
 
+var confirm=false;
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+    const app = initializeApp(firebaseConfig);
 
-import {getDatabase, ref, set, child, get,update, remove} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js" 
+    import {getDatabase, ref, set, child, get,update, remove} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js" 
 
-const db= getDatabase();
+    const db= getDatabase();
 
 
-    const dbref= ref(db);
+        const dbref= ref(db);
 
-    get(child(dbref,"instructor")).then((snapshot)=>{
-        if (snapshot.exists()) {
-            return display(snapshot.val());
-        }
-        else {
-            alert("No data found")
-        }
-    })
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id')
-    // Id is ali
+        get(child(dbref,"instructor")).then((snapshot)=>{
+            if (snapshot.exists() && confirm==true) {
+                return display(snapshot.val());
+            }
+            else {
+                alert("No data found")
+            }
+        })
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const id = urlParams.get('id')
+        // Id is ali
+        
+var instr_img = document.getElementById("instructor-img");
+var instr_name = document.getElementById("instructor-name");
+
+instr_name.innerText = id;
+instr_img.src = `../images/${id}.JPG`;
 
 
 function display(input){
-    var date_select = document.getElementById("selected_date")
-    console.log(date_select)
-    for(let ele in input){
-        if(ele == id){
-            console.log(input)
-            var avail_dates = ele.dates
-            // assume can get date
+    var date_select = document.getElementById("selected_date").innerText;
+    var date_split = date_select.split(" ").join('')
+    var date_format_needed = date_split.split('/').join('-');
+    console.log(date_format_needed);
+    var count=0;
+    var string =""
+    if(confirm==true){
+        for(let ele in input){
+            if(ele == id){
+                console.log(input)
+                var avail_dates = input[ele].date
+                // assume can get date
+                console.log(avail_dates)
+                for(let a_date in avail_dates){
+                    if (a_date == date_format_needed){
+                        var time = avail_dates[a_date]
+                    }
+                }
+            }
         }
     }
+    
+
+    if(time.length>0){
+        for(let timings of time){
+            console.log(timings)
+            string+= `<button class="button-18" role="button"> ${timings}</button> `;
+        }
+    }
+    console.log(string)
+    var to_update = document.getElementById("times");
+    to_update.innerHTML = string;
 }
