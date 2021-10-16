74e6909c51f59b26e4a1b1a0ec730ded57d054d9
Bak Wen Yan <bakwenyan@gmail.com>
2021-10-16 18:12:16 +0800

    Create post.js
    Created function to push new post data to the database. Need to work on putting current user into the data as well.

---
+++ js/post.js
@@ -0,0 +1,37 @@
+var config = 
+{
+    apiKey: "AIzaSyDruPuuruiIkp6rGikLAUixFbT5-RFRm0s",
+    authDomain: "wad2-e8948.firebaseapp.com",
+    databaseURL: "https://wad2-e8948-default-rtdb.asia-southeast1.firebasedatabase.app",
+    storageBucket: "wad2-e8948.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+function create_new_post(title,content,creator,tag,time)
+{
+    var rootRef = firebase.database().ref();
+    var storesRef = rootRef.child('post');
+    var newStoreRef = storesRef.push();
+    newStoreRef.set({
+        "title": title,
+        "content": content,
+        "creator": creator,
+        "tag": tag,
+        "time": time,
+    }
+
+   , (error) => {
+        if (error) {
+         // The write failed...
+            alert("There was a problem with post creating")
+        } 
+
+        else {
+         // Data saved successfully!
+            alert("Post creation is successful")
+            window.location.href = "add_post.html";
+        }
+    });
+    }
+
