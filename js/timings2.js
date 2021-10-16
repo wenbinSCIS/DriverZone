6f504b25d9fa81968d31bb7c7410afa6c6594ae0
Ho-JE <juayen.ho.2020@scis.smu.edu.sg>
2021-10-16 17:49:09 +0800

    Create timings2.js

---
+++ js/timings2.js
@@ -0,0 +1,70 @@
+
+var config = {
+    apiKey: "AIzaSyDruPuuruiIkp6rGikLAUixFbT5-RFRm0s",
+    authDomain: "wad2-e8948.firebaseapp.com",
+    databaseURL: "https://wad2-e8948-default-rtdb.asia-southeast1.firebasedatabase.app",
+    storageBucket: "wad2-e8948.appspot.com",
+    };
+
+firebase.initializeApp(config);
+//get instructor info from course list
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id')
+// Id is ali
+
+//button stuff and initialize databse
+var select = document.getElementById("confirm")
+select.addEventListener("click", displayTimings);
+
+var currentSnapshot;
+
+function getDB(){
+    var ref = firebase.database().ref('instructor');
+    return ref.once("value");
+}
+function loadDB(){
+    getDB().then(setDB,showError);
+}
+function setDB(snapshot){
+    currentSnapshot = snapshot.val()
+}
+function showError(e){
+    console.log(e);
+}
+function init(){
+    loadDB();
+}
+
+
+
+
+function displayTimings(database){
+    var date_select = document.getElementById("selected_date").innerText;
+    var date_split = date_select.split(" ").join('')
+    var date_format_needed = date_split.split('/').join('-');
+    console.log(date_format_needed);
+
+    for(let ele in database){
+        if(ele == id){
+            console.log(input)
+            var avail_dates = input[ele].date
+            // assume can get date      
+            console.log(avail_dates)
+            for(let a_date in avail_dates){
+                if (a_date == date_format_needed){
+                    var time = avail_dates[a_date]
+                    console.log(time)
+                }
+            }
+        }
+    }
+    if(time.length>0){
+    for(let timings of time){
+        console.log(timings)
+        string+= `<button class="button-18" role="button"> ${timings}</button> `;
+    }
+    }
+    var to_update = document.getElementById("times");
+    to_update.innerHTML = string;
+    } 
