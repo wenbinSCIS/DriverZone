286d7ce2830416cb9489140ab7b7628233a21801
Bak Wen Yan <bakwenyan@gmail.com>
2021-10-17 15:51:22 +0800

    Update post.js

--- js/post.js
+++ js/post.js
@@ -19,7 +19,14 @@ function create_new_post(title,content,creator,tag,time, no_of_vote)
         "creator": creator,
         "tag": tag,
         "time": time,
-        "no_of_vote": no_of_vote
+        "no_of_vote": no_of_vote,
+        "comment": 
+        {
+            "0":{
+                "content":"This is the beginning of the post",
+                "user_id":"System"
+            }
+        }
     }
 
    , (error) => {
@@ -34,5 +41,70 @@ function create_new_post(title,content,creator,tag,time, no_of_vote)
             window.location.href = "forum.html";
         }
     });
-    }
+}
 
+function get_username(cur_user_id)
+{
+    var rootRef = firebase.database().ref();
+    var storesRef = rootRef.child('users');
+    var count = 0;
+    var total_count = 0;
+    let cur_name=null
+
+    storesRef.on("value", function(snapshot) 
+    {
+        snapshot.forEach((child) => 
+        {
+            total_count=total_count+1
+
+            if (cur_user_id == child.key)
+            {
+                cur_name=child.val().name;
+            }
+
+            else
+            {
+                count++
+            }
+
+        })
+
+        if (count == total_count)
+        {
+            console.log("no such user")
+        }
+        console.log(cur_name)
+        console.log("____")
+        return cur_name
+    })
+}
+
+function get_all_post()
+{
+    var rootRef = firebase.database().ref();
+    var storesRef = rootRef.child('post');
+
+    storesRef.on("value", function(snapshot)
+    {
+        var all_post_list = []
+        for( var item in snapshot.val())
+        {
+            var cur_title = snapshot.val()[item].title
+            var cur_content = snapshot.val()[item].content
+            var cur_creator_id = snapshot.val()[item].creator
+            var cur_name = get_username(cur_creator_id)
+            var cur_tag = snapshot.val()[item].tag
+            var cur_time = snapshot.val()[item].time
+            var cur_upvote = snapshot.val()[item].no_of_vote
+            var cur_comment = snapshot.val()[item].comment
+
+            all_post_list.push([cur_title,cur_content,cur_creator_id,cur_name,cur_tag,cur_time,cur_upvote,cur_comment])
+        }
+
+        console.log(all_post_list)
+
+        return all_post_list
+    })
+
+    return all_post_list
+}
