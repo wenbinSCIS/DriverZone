c9c7352ff70ca4d4741b7076f9eb10e202b08ea2
Ho-JE <juayen.ho.2020@scis.smu.edu.sg>
2021-11-02 15:59:26 +0800

    booking updated

--- js/bookingv2.js
+++ js/bookingv2.js
@@ -16,60 +16,42 @@ const id = urlParams.get('id')
 const user_id = sessionStorage.getItem("userid");
 
 //get timings to be in Calendar
-var allEvents =[];
+
 var string=""
 var ref = firebase.database().ref(`instructor/${id}/date`); 
 ref.once("value")
     .then(function(snapshot){
         var instr_db = snapshot.val()
+        var allEvents =[];
         for(let date in instr_db){
-            var object = instr_db[date];
-            object.id = date;
+          for(let obj in instr_db[date]){
+            var object = instr_db[date][obj];
+            console.log(object)
+            object.id = Math.floor(Math.random() * 100000000000000000000000000000000000000000000000) + 1;
             allEvents.push(object)
+            }
+            
+          
+                
+          }
             $("#calendar").evoCalendar({
               theme: 'Royal Navy',
               'eventListToggler': false,
               'sidebarToggler': false,
               calendarEvents: allEvents,
-              /* calendarEvents: [
-                  {
-                  name: "Lesson",
-                  id:1234,
-                    badge: "$50", // Event badge (optional)
-                    date: "2/13/2020", 
-                    description: "1pm - 2pm", // Event description (optional)
-                    type: "event",
-                    color: "#222831" // Event custom color (optional)
-                  },
-                  {
-                      name: "Lesson",
-                      id:12345,
-                      badge: "$50", // Event badge (optional)
-                      date: "2/13/2020", 
-                      description: "2pm - 3pm", // Event description (optional)
-                      type: "event",
-                      color: "#222831" // Event custom color (optional)
-                    },
-                    {
-                      name: "Lesson",
-                      id:123456,
-                      badge: "$50", // Event badge (optional)
-                      date: "2/13/2020", 
-                      description: "2pm - 3pm", // Event description (optional)
-                      type: "event",
-                      color: "#222831",
-                    }
-                ] */
+            })
+    });
           
-                
-            });
-          
-        }
-    })        
 
   $('#calendar').on('selectEvent', function(event, activeEvent) {
         // $('#calendar').evoCalendar('toggleEventList', false);
-      
+    var active_date = $('#calendar').evoCalendar('getActiveDate');
+    sessionStorage.setItem("instructor",id);
+    sessionStorage.setItem("date",active_date)
+    sessionStorage.setItem("time",activeEvent.description)
+    console.log(activeEvent)
+    sessionStorage.setItem("cost",activeEvent["badge"])
+    window.location.href = "confirm_booking.html";
     });
 
 
@@ -81,12 +63,13 @@ ref.once("value")
         console.log(unix_needed)   
         var unixTimeStamp = parseInt((new Date(unix_needed).getTime() / 1000).toFixed(0))
         var ref = firebase.database().ref(`instructor/${id}/date/${unixTimeStamp}`);
-        ref.set({
+        var newStoreRef = ref.push();
+        newStoreRef.set({
                 "badge":"$50",
                 "color":"#222831",
                 "date":active_date,
                 "description":time_to_add,
-                name:"Driving Lesson",
+                name:"Driving lesson",
                 type:"event",
         }
         , (error) => {
